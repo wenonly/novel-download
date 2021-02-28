01b2378a35b4c94a32bac31a620b40bbc09c0b65
Taowen <1325772089@qq.com>
2021-02-28 14:35:11 +0800

    build: 开发打包结果不用eval

--- scripts/config/webpack.dev.js
+++ scripts/config/webpack.dev.js
@@ -4,7 +4,7 @@ const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = merge(common, {
   mode: "development",
-  devtool: "eval-cheap-module-source-map",
+  devtool: "cheap-module-source-map",
   watch: true,
   plugins: [
     // * 打包前清理上一次的 dist 文件夹。
